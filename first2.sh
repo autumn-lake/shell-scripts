a691e29596a58a4c1fdcf20990560b11e2e0b572
autumn-lake <autumn.lake.00@gmail.com>
2017-08-22 20:17:22 +0530

    predict outcome of echo command

---
+++ first2.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# this is a comment
+echo "Hello      World" # This is a comment, too!
+echo "Hello World"
+echo "Hello * World"
+echo Hello * World
+echo Hello      World
+echo "Hello" World
+echo Hello "      " World
+echo "Hello "*" WOrld"
+echo `hello` world
+echo 'hello' world
